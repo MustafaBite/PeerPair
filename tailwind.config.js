55231acaf1ec78900d8c755aafab90303ec773a7
sinemturkoglu9 <turkoglusinem5@gmail.com>
2025-06-05 23:48:06 +0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-const { themeColors, themeAnimations } = require('./src/config/theme.config');
-
 module.exports = {
-  darkMode: 'class',
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: [],
   theme: {
-    extend: {
-      colors: themeColors.light,
-      animation: {
-        'fade-in': 'fadeIn 0.5s ease-in-out',
-        'slide-up': 'slideUp 0.5s ease-out',
-      },
-      keyframes: themeAnimations,
-    },
+    extend: {},
   },
   plugins: [],
-} 
+}
+
